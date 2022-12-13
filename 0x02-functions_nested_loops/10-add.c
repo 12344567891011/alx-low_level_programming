bc7354269661750f34f25884bb5198638ecf8a00
asmae <117948441+12344567891011@users.noreply.github.com>
2022-12-13 17:44:18 +0100

    done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - add 2 int
+ * @n1: number 1
+ * @n2: number 2
+ * Description: results
+ * Return: Result
+ */
+
+int add(int n1, int n2)
+{
+	int result = n1 + n2;
+return (result);
+}
